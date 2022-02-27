26e5cb421c77c4c49ab25637d687f784faabfad2
pamodasilva99 <96543@fhss.sjp.ac.lk>
2022-02-27 13:23:08 +0530

    Updates

---
+++ precache-manifest.8eccdfcf518bd72066c7371742e2e58f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4078f0c0d451270b9c66e0b8ba971d56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e2b49f771716f1762e4",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "a320587fa11d43500755",
+    "url": "/static/css/main.da5a15cc.chunk.css"
+  },
+  {
+    "revision": "3e2b49f771716f1762e4",
+    "url": "/static/js/2.649c0d49.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/static/js/2.649c0d49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a320587fa11d43500755",
+    "url": "/static/js/main.e823f923.chunk.js"
+  },
+  {
+    "revision": "e50b541b3ebb27c081ba",
+    "url": "/static/js/runtime-main.0035dbdf.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "ee20d8647ef811f1eb5f7a9f538d3e6d",
+    "url": "/static/media/Photo.ee20d864.jpeg"
+  },
+  {
+    "revision": "57f0514feba30717a3135093e829baab",
+    "url": "/static/media/Pic01.57f0514f.png"
+  },
+  {
+    "revision": "ffebfc0b4fd64e46461681a621205232",
+    "url": "/static/media/Pic02.ffebfc0b.png"
+  },
+  {
+    "revision": "58455a07d917375f836f39eee1aba6c6",
+    "url": "/static/media/Pic03.58455a07.png"
+  }
+]);
